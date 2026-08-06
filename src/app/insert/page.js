8c35d6ece9431ba32e952fd50371b5f7dbe6f9ae
alikerock <alikerock@gmail.com>
2026-08-06 11:52:01 +0900

    feat: 파일명 난수 생성 업로드

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -44,6 +44,7 @@ export default function Insert() {
     } else {
       console.log("데이터 입력 성공");
       router.push("/");
+      router.refresh();
     }
     if (thumbnail) {
       await uploadThumbnail(thumbnail);
@@ -65,12 +66,16 @@ export default function Insert() {
 
   const handleFileChange = e => {
     setThumbnail(e.target.files[0]);
+    console.log(e.target.files[0]);
   };
 
   async function uploadThumbnail(file) {
+    const ext = file.name.split(".").pop();
+    const fileName = `${crypto.randomUUID()}.${ext}`;
+
     const { data, error } = await supabase.storage
       .from("portfolio")
-      .upload(`thumbnail/${file.name}`, file);
+      .upload(`thumbnail/${fileName}`, file);
     if (error) {
       // Handle error
       console.error("파일 업로드 실패:", error);
@@ -87,6 +92,7 @@ export default function Insert() {
       alert("로그인 실패", error.message);
     } else {
       alert("로그인 성공");
+      setUser(data.user);
       router.refresh();
     }
   };
