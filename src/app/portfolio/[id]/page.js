5ae73646e30bb72943cfe4c9275cf2cf7485906a
alikerock <alikerock@gmail.com>
2026-08-10 13:39:46 +0900

    feat: 대표이미지 출력

--- src/app/portfolio/[id]/page.js
+++ src/app/portfolio/[id]/page.js
@@ -1,10 +1,25 @@
 import { createClient } from "@/utils/supabase/client";
+import Image from "next/image";
 
 export default async function Portfolio({ params }) {
   const supabase = createClient();
   const { id } = await params;
 
-  const { data, error } = await supabase.from("portfolio").select().eq("id", id).single();
+  const { data: current, error } = await supabase
+    .from("portfolio")
+    .select(
+      `*,
+      portfolio_images(
+      id,
+      image_url,
+      description,
+      display_order
+      )
+    `,
+    )
+    .eq("id", id)
+    .order("display_order", { referencedTable: "portfolio_images", ascending: true })
+    .single();
 
   //이전글 id, title 조회
   const { data: prev } = await supabase
@@ -24,33 +39,51 @@ export default async function Portfolio({ params }) {
     .limit(1)
     .maybeSingle();
 
-  console.log("prev" + prev);
-  console.log("next" + next);
+  // console.log("prev" + prev);
+  // console.log("next" + next);
+  // console.log(current);
+
+  const getPublicURL = path => {
+    if (!path) return "";
+    const { data: publicUrlData } = supabase.storage.from("portfolio").getPublicUrl(path);
+    return publicUrlData.publicUrl;
+  };
+
+  const portfolioImages = current.portfolio_images ?? [];
 
   return (
     <div className="portoflio-single">
       <div className="row">
         <div className="col-md-8 decription">
-          <div className="contents shadow">
-            {/* <img src="images/portfolio_single_img1.jpg" alt="img1"> */}
-            <p>{data?.rep1_desc ?? ""}</p>
-          </div>
-          <div className="contents shadow">
-            {/* <img src="images/portfolio_single_img2.jpg" alt="img2"> */}
-            <p>{data?.rep2_desc ?? ""}</p>
-          </div>
+          {portfolioImages.length > 0 ? (
+            portfolioImages.map((image, idx) => (
+              <div key={idx} className="contents shadow">
+                <Image
+                  src={getPublicURL(image.image_url)}
+                  alt={image.description}
+                  width={762}
+                  height={504}
+                  style={{ width: "100%", height: "auto" }}
+                  loading="eager"
+                />
+                <p>{image.description}</p>
+              </div>
+            ))
+          ) : (
+            <div className="contents shadow">대표 이미지가 없습니다.</div>
+          )}
         </div>
         <div className="col-md-4 portfolio_info">
           <div className="contents shadow">
-            <h2>{data?.title ?? "Project Title"}</h2>
-            <div>{data?.content ?? ""}</div>
+            <h2>{current?.title ?? "Project Title"}</h2>
+            <div>{current?.content ?? ""}</div>
             <p className="link">
-              <a href={data?.url ?? ""}>Visit site &rarr;</a>
+              <a href={current?.url ?? ""}>Visit site &rarr;</a>
             </p>
             <hr className="double" />
             <blockquote>
-              <p>{data?.review ?? ""}</p>
-              <small>- {data?.reviewer ?? ""} -</small>
+              <p>{current?.review ?? ""}</p>
+              <small>- {current?.reviewer ?? ""} -</small>
             </blockquote>
             <p className="nav">
               {prev && (
